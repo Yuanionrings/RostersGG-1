5f3ad483bac6d8e2843f99c4456cbecc58ae317e
Owen <moreauowen@gmail.com>
2020-08-11 18:57:14 -0400

    EventCard props

--- client/src/components/events/EventCard.js
+++ client/src/components/events/EventCard.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-function EventCard(){
-    console.log(this.props);
+function EventCard(props){
+    console.log(props);
     return (
         <div className="event-card">
-            <h4>{this.props.name}</h4>
-            <p>{this.props.description}</p>
-            <p>{this.props.when}</p>
+            <h4>{props.name}</h4>
+            <p>{props.description}</p>
+            <p>{props.when}</p>
             <hr />
             <p>Event starts in: </p>
             <p>Calculated time here</p>
