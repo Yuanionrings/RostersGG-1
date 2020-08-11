468c19f3de94d6a8bc90cdf1828e3aa72885835f
Owen <moreauowen@gmail.com>
2020-08-10 21:22:32 -0400

    Footer style changes

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -8,9 +8,9 @@ function Footer(){
             </div>*/}
             <div className="footer-bottom primary-bg-dark">
                 <p className="secondary-text-light">
-                    <a href='https://www.rosters.gg/' target="_blank">Terms of Service</a>
-                    <a href='https://www.rosters.gg/' target="_blank">Privacy</a>
-                    <a href='https://www.rosters.gg/' target="_blank">Cookies</a>
+                    <a href='https://www.rosters.gg/legal/terms-and-conditions'>Terms and Conditions</a>
+                    <a href='https://www.rosters.gg/legal/privacy-policy'>Privacy Policy</a>
+                    <a href='https://www.rosters.gg/'>Cookies</a>
                      © 2020 Rosters.gg
                 </p>
             </div>
