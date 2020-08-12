1ded50462c8d55326ebbe8adf4be116967b000cd
Owen <moreauowen@gmail.com>
2020-08-11 22:45:30 -0400

    should be good now

--- client/src/components/events/EventCard.js
+++ client/src/components/events/EventCard.js
@@ -2,16 +2,17 @@ import React from 'react';
 import calcTimeUntil from '../../util/calcTimeUntil';
 import dateFormat from 'dateformat';
 
+function formatDateString(dateISO) {
+    const date = new Date(dateISO);
+    return dateFormat(date, "dddd, mmmm dS, yyyy, h:MM:ss TT");
+};
+
 function EventCard(props){
-    formatDateString = () => {
-        const date = new Date(props.when);
-        return dateFormat(date, "dddd, mmmm dS, yyyy, h:MM:ss TT");
-    };
     return (
         <div className="event-card">
             <h4>{props.name}</h4>
             <p className="mb-0">{props.description}</p>
-            <p className="mt-0 filler-text">{this.formatDateString()}</p>
+            <p className="mt-0 filler-text">{formatDateString(props.when)}</p>
             <hr />
             <p className="filler-text">
                 <strong>Time until event: </strong>{calcTimeUntil(new Date(props.when), Date.now())}
