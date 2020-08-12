882cb51ce4e3cc4ef2edb02a0e17a687b5589665
Owen <moreauowen@gmail.com>
2020-08-12 14:21:23 -0400

    Styling changes

--- client/src/components/events/EventCard.js
+++ client/src/components/events/EventCard.js
@@ -5,10 +5,10 @@ import { formatDateString } from '../../util/formatDateString';
 function EventCard(props){
     return (
         <div className='event-card'>
-            <h4 className='mb-0'>{props.name}</h4>
-            <p className='mt-0 mb-0'>{props.description}</p>
-            <p className='mt-0 mb-1'>Associated Teams: <span className='filler-text'>{props.team_names}</span></p>
-            <p className='mt-2 filler-text'>{formatDateString(props.when)}</p>
+            <h5 className='mb-0'>{props.name}</h5>
+            <p className='mt-0 mb-2'>{props.description}</p>
+            <p className='mt-2 mb-0'>Associated Teams: <span className='filler-text'>{props.team_names}</span></p>
+            <p className='mt-0 filler-text'>{formatDateString(props.when)}</p>
             <hr />
             <p className='filler-text'>
                 <strong>Time until event: </strong>{calcTimeUntil(new Date(props.when), Date.now())}
