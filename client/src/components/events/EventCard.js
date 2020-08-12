66715895422262ef0e8a06f7440da249c443815e
Owen <moreauowen@gmail.com>
2020-08-11 22:38:13 -0400

    Date styling

--- client/src/components/events/EventCard.js
+++ client/src/components/events/EventCard.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import calcTimeUntil from '../../util/calcTimeUntil';
+import dateFormat from 'dateformat';
 
 function EventCard(props){
+    formatDateString = () => {
+        const date = new Date(props.when);
+        return dateFormat(date, "dddd, mmmm dS, yyyy, h:MM:ss TT");
+    };
     return (
         <div className="event-card">
             <h4>{props.name}</h4>
             <p className="mb-0">{props.description}</p>
-            <p className="mt-0">{props.when}</p>
+            <p className="mt-0 filler-text">{formatDateString()}</p>
             <hr />
             <p className="filler-text">
                 <strong>Time until event: </strong>{calcTimeUntil(new Date(props.when), Date.now())}
