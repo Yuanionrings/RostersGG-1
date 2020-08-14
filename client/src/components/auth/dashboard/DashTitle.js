c777580fd0866ab7b8da1f03975a5e4b6a9eabbc
Owen <moreauowen@gmail.com>
2020-08-14 09:15:36 -0400

    Title box and footer style changes

--- client/src/components/auth/dashboard/DashTitle.js
+++ client/src/components/auth/dashboard/DashTitle.js
@@ -3,7 +3,7 @@ import React from 'react';
 const DashTitle = props => {
     return (
         <div className='display-box'>
-            <div className='box'>
+            <div className='title-box'>
                 <h2 className=''>
                     {props.page_title}
                 </h2>
