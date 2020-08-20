549602f8b33bc8d9796dccbd621e9a1063092be4
Owen <moreauowen@gmail.com>
2020-08-19 21:10:00 -0400

    Not Found style changes

--- client/src/components/general/NotFound.js
+++ client/src/components/general/NotFound.js
@@ -10,10 +10,10 @@ export default class NotFound extends Component {
                 <DashHeader />
                 <div className='text-page-container not-found'>
                     <h1>404 Not Found</h1>
-                    <p>Sorry about that! We could not find what you were looking for.</p>
+                    <p>Such a gamer moment! We could not find what you were looking for.</p>
 
-                    <h3>Please go to <a href='https://www.rosters.gg'>Rosters.gg</a> to continue accessing the platform.</h3>
-                    <h4>If this error persists, shoot an email to contact@rosters.gg and a team member will reach out to you.</h4>
+                    <h5>Please go to <a href='https://www.rosters.gg'>Rosters.gg</a> to continue accessing the platform.</h5>
+                    <p>If this error persists, shoot an email to contact@rosters.gg and a team member will reach out to you.</p>
                 </div>
                 <DashFooter />
             </div>
