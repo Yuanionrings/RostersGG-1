7667608aefc774ec991d96333ee3c71b94dc5087
Owen <moreauowen@gmail.com>
2020-08-22 19:47:59 -0400

    Fix icons and default profile image on player directory

--- client/src/components/auth/user/UserListingCard.js
+++ client/src/components/auth/user/UserListingCard.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import { getCorrectPath } from '../../../util/developmentHelper';
 
 import InvitationIcon from '../../../assets/icons/send-invitation.svg';
-
+import DefaultProfile from '../../../assets/images/default_profile.svg';
 
 function displayRostersInModal(auth, username_to_invite, history, rosters) {
     if(rosters) {
@@ -62,11 +62,15 @@ const UserListingCard = props => {
 
     return (
         <div className='user-listing-card'>
-            <div className='text'>
-                <h5 className='mb-0'>
-                    <Link to={`/user/${props.user.username}`}>{props.user.name}</Link>
-                </h5>
-                <p className='filler-text'>@{props.user.username}</p>
+            <div className='image-container'>
+                <img src={DefaultProfile} alt='Default RostersGG Profile Icon' />
+            
+                <div className='text'>
+                    <h5 className='mb-0'>
+                        <Link to={`/user/${props.user.username}`}>{props.user.name}</Link>
+                    </h5>
+                    <p className='filler-text'>@{props.user.username}</p>
+                </div>
             </div>
             
             <button className='invite-button' onClick={() => setModalIsOpen(true)}>
